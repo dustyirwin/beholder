8633d626da1d4c7c9c177cf50a016c792745f066
dusty <dusty.irwin@gmail.com>
2018-05-05 18:48:55 -0700

    sourcing init

--- sourcing/views.py
+++ sourcing/views.py
@@ -4,10 +4,4 @@ from pricing.models import *
 import mws
 
 def index(request):
-    products_api = mws.Products(access_key='AKIAJGZE5C5XUOCSORYQ',
-                                secret_key='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',
-                                account_id='A3A3KAZ6LDQG83',
-                                )
-    products = products_api.get_lowest_offer_listings_for_asin('US','B00009WAU9')
-    print(products.parsed)
     return render(request,'sourcing/index.html')
